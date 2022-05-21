82f8b9d7a2b20c599acd507f1d08444db02c64a0
Rafael-Cobar <rafaelcobarg@gmail.com>
2022-05-20 22:53:55 -0600

    [ADD] Inicializar Observador de Carpetas C#

---
+++ ObservadorCarpetas/ObservadorCarpetas/Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace ObservadorCarpetas
+{
+    partial class ObservadorCarpetas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ObservadorCarpetas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 500);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "ObservadorCarpetas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ObservadorCarpetas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
